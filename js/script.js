4216a4958311f8ac8080e0ac9893e4c8826ef763
luis.gomez2 <luis.gomez@escuelaestech.es>
2019-05-28 18:40:01 +0200

    Primer avance

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+
+	    //DATAPICKER - Fecha A
+	 
+
+	  
+	/* Barra progreso */
+
+	$( function() {
+	  $( "#progressbar" ).progressbar({
+	    value: 37
+	  });
+	});
+
+	/* Acordeon */
+
+
+	 $(document).ready(function(){
+	      $("#botonMasOpciones").mouseover('slow',function(){
+	        $(".masOpciones").toggle("slow" );
+	      });
+	    });
+
+	   
+
+
+
+
+
+
+
